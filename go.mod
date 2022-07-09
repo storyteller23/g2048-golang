4bac1b4c5c9f923a6a897bee0ae7bf624780ac6a
storyteller <storyteller0205@gmail.com>
2022-07-09 16:30:16 +0600

    can play

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/storyteller23/g2048-golang
+
+go 1.17
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/eiannone/keyboard"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	area := createArea()
+	for {
+		getTwoOfFour(area)
+		displayArea(area)
+		for {
+			char, _, err := keyboard.GetSingleKey()
+			if err != nil {
+				panic(err)
+			}
+			switch string(char) {
+			case "w":
+				toUp(area)
+			case "s":
+				toDown(area)
+			case "a":
+				toLeft(area)
+			case "d":
+				toRight(area)
+			case "q":
+				return
+			default:
+				continue
+			}
+			break
+		}
+	}
+}
+
+func createArea() [][]int {
+	res := make([][]int, 4)
+	for i := 0; i < len(res); i++ {
+		res[i] = append(res[i], make([]int, 4)...)
+	}
+	return res
+}
+
+func displayArea(area [][]int) {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("o--------o--------o--------o--------o")
+	for _, row := range area {
+		fmt.Println("|        |        |        |        |")
+		fmt.Printf("|%5d   |%5d   |%5d   |%5d   |\n", row[0], row[1], row[2], row[3])
+		fmt.Println("|        |        |        |        |")
+		fmt.Println("o--------o--------o--------o--------o")
+	}
+}
+
+func zerosCoord(area [][]int) [][]int {
+	res := make([][]int, 0)
+	for i := 0; i < len(area); i++ {
+		for j := 0; j < len(area); j++ {
+			if area[i][j] == 0 {
+				res = append(res, []int{i, j})
+			}
+		}
+	}
+	return res
+}
+
+func getTwoOfFour(area [][]int) {
+	zeros := zerosCoord(area)
+	if len(zeros) == 0 {
+		return
+	}
+	choice := rand.Intn(len(zeros))
+	if rand.Intn(2) == 0 {
+		area[zeros[choice][0]][zeros[choice][1]] = 2
+	} else {
+		area[zeros[choice][0]][zeros[choice][1]] = 4
+	}
+}
+
+func toUp(area [][]int) {
+	for k := 0; k < len(area); k++ {
+		for i := 0; i < len(area); i++ {
+			for j := 1; j < len(area); j++ {
+				if area[j-1][i] == 0 && area[j][i] != 0 {
+					area[j-1][i], area[j][i] = area[j][i], area[j-1][i]
+				}
+			}
+		}
+	}
+	for i := 0; i < len(area); i++ {
+		for j := 1; j < len(area); j++ {
+			if area[j-1][i] == area[j][i] {
+				area[j-1][i] = area[j-1][i] + area[j][i]
+				area[j][i] = 0
+			}
+		}
+	}
+	for k := 0; k < len(area); k++ {
+		for i := 0; i < len(area); i++ {
+			for j := 1; j < len(area); j++ {
+				if area[j-1][i] == 0 && area[j][i] != 0 {
+					area[j-1][i], area[j][i] = area[j][i], area[j-1][i]
+				}
+			}
+		}
+	}
+}
+
+func toDown(area [][]int) {
+	for k := 0; k < len(area); k++ {
+		for i := len(area) - 1; i >= 0; i-- {
+			for j := len(area) - 2; j >= 0; j-- {
+				if area[j+1][i] == 0 && area[j][i] != 0 {
+					area[j+1][i], area[j][i] = area[j][i], area[j+1][i]
+				}
+			}
+		}
+	}
+	for i := len(area) - 1; i >= 0; i-- {
+		for j := len(area) - 2; j >= 0; j-- {
+			if area[j+1][i] == area[j][i] {
+				area[j+1][i] = area[j+1][i] + area[j][i]
+				area[j][i] = 0
+			}
+		}
+	}
+	for k := 0; k < len(area); k++ {
+		for i := len(area) - 1; i >= 0; i-- {
+			for j := len(area) - 2; j >= 0; j-- {
+				if area[j+1][i] == 0 && area[j][i] != 0 {
+					area[j+1][i], area[j][i] = area[j][i], area[j+1][i]
+				}
+			}
+		}
+	}
+}
+
+func toRight(area [][]int) {
+	for k := 0; k < len(area); k++ {
+		for i := len(area) - 1; i >= 0; i-- {
+			for j := len(area) - 2; j >= 0; j-- {
+				if area[i][j+1] == 0 && area[i][j] != 0 {
+					area[i][j+1], area[i][j] = area[i][j], area[i][j+1]
+				}
+			}
+		}
+	}
+	for i := len(area) - 1; i >= 0; i-- {
+		for j := len(area) - 2; j >= 0; j-- {
+			if area[i][j+1] == area[i][j] {
+				area[i][j+1] = area[i][j+1] + area[i][j]
+				area[i][j] = 0
+			}
+		}
+	}
+	for k := 0; k < len(area); k++ {
+		for i := len(area) - 1; i >= 0; i-- {
+			for j := len(area) - 2; j >= 0; j-- {
+				if area[i][j+1] == 0 && area[i][j] != 0 {
+					area[i][j+1], area[i][j] = area[i][j], area[i][j+1]
+				}
+			}
+		}
+	}
+}
+
+func toLeft(area [][]int) {
+	for k := 0; k < len(area); k++ {
+		for i := 0; i < len(area); i++ {
+			for j := 1; j < len(area); j++ {
+				if area[i][j-1] == 0 && area[i][j] != 0 {
+					area[i][j-1], area[i][j] = area[i][j], area[i][j-1]
+				}
+			}
+		}
+	}
+	for i := 0; i < len(area); i++ {
+		for j := 1; j < len(area); j++ {
+			if area[i][j-1] == area[i][j] {
+				area[i][j-1] = area[i][j-1] + area[i][j]
+				area[i][j] = 0
+			}
+		}
+	}
+
+	for k := 0; k < len(area); k++ {
+		for i := 0; i < len(area); i++ {
+			for j := 1; j < len(area); j++ {
+				if area[i][j-1] == 0 && area[i][j] != 0 {
+					area[i][j-1], area[i][j] = area[i][j], area[i][j-1]
+				}
+			}
+		}
+	}
+}
